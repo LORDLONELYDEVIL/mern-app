9fd68795587b152a9afcde5437aa9f3d30214583
LORDLONELYDEVIL <lordlonelydevil001@gmail.com>
2024-02-22 19:12:45 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
+import path from "path";
+
 import authroute from "./routes/authroute.js";
 import connectDB from "./db/connectDB.js";
 import messageRoute from "./routes/messageRoute.js";
@@ -16,6 +18,8 @@ dotenv.config();
 
 const PORT=process.env.PORT || 5000;
 
+const __dirname=path.resolve();
+
 
 app.use(express.json()); //parse the incoming requests to json payloads(from req.body)
 app.use(cookieParser());
@@ -23,6 +27,11 @@ app.use(cookieParser());
 app.use("/api/auth",authroute);
 app.use("/api/messages",messageRoute);
 app.use("/api/users",userRoutes);
+
+app.use(express.static(path.join(__dirname,"/frontend/dist")));
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"));
+});
 
 
 server.listen(PORT,()=>{
